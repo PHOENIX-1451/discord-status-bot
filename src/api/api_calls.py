74ffef4e532f4a5f96a5bac354fd1ddf5acb330b
PHOENIX <phoenix1451.gaming@gmail.com>
2025-08-28 01:47:15 +0100

    API call to fetch server data

--- src/api/api_calls.py
+++ src/api/api_calls.py
@@ -1,6 +1,7 @@
 import os
 import httpx
 
+from src.api import APIClient
 from src.api.api_mappings import APIMappings
 from src.authorisation import Authorisation
 
@@ -8,7 +9,7 @@ from src.authorisation import Authorisation
 class APICalls:
 
     @staticmethod
-    async def get_server_data(self):
+    async def get_server_data():
         try:
             data = {
                 "url": APIMappings.BASE_URL + APIMappings.ENDPOINTS["getServerData"],
@@ -17,6 +18,11 @@ class APICalls:
                     "usid": os.getenv("USID")
                 }
             }
+            # Send request
+            api_client = APIClient()
+            server_data =  await api_client.get(data)
+            # Return request
+            return server_data.json()
         except httpx.HTTPStatusError as hse:
             print(hse)
         except Exception as e:
