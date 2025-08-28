fd332c33994c30e32b156a43f0e69e29ccf468ce
PHOENIX <phoenix1451.gaming@gmail.com>
2025-08-28 19:10:55 +0100

    create file mappings

---
+++ src/utilities/mappings.py
@@ -0,0 +1,37 @@
+
+class Mappings:
+
+    MAP_FILE_MAPPINGS = {
+        "Achi Baba": "Achi Baba.jpg",
+        "Albion": "Albion.jpg",
+        "Amiens": "Amiens.jpg",
+        "Argonne Forest": "Argonne Forest.jpg",
+        "Ballroom Blitz": "Ballroom Blitz.jpg",
+        "Brusilov Keep": "Brusilov Keep.jpg",
+        "Cape Helles": "Cape Helles.jpg",
+        "Caporetto": "Caporetto.jpg",
+        "Empire's Edge": "Empire's Edge.jpg",
+        "Fao Fortress": "Fao Fortress.jpg",
+        "Fort De Vaux": "Fort De Vaux.jpg",
+        "Galicia": "Galicia.jpg",
+        "Giant's Shadow": "Giant's Shadow.jpg",
+        "Heligoland Bight": "Heligoland Bight.jpg",
+        "London Calling: Raiders": "London Calling- Raiders.jpg",
+        "London Calling: Scourage": "London Calling- Scourage.jpg",
+        "Łupków Pass": "Łupków Pass.jpg",
+        "Monte Grappa": "Monte Grappa.jpg",
+        "Nivelle Nights": "Nivelle Nights.jpg",
+        "Passchendael": "Passchendael.jpg",
+        "Prise De Tahure": "Prise De Tahure.jpg",
+        "Razor's Edge": "Razor's Edge.jpg",
+        "River Somme": "River Somme.jpg",
+        "Rupture": "Rupture.jpg",
+        "Sinai Desert": "Sinai Desert.jpg",
+        "Soissons": "Soissons.jpg",
+        "St Quentin Scar": "St Quentin Scar.jpg",
+        "Suez": "Suez.jpg",
+        "Tsaritsyn": "Tsaritsyn.jpg",
+        "Verdun Heights": "Verdun Heights.jpg",
+        "Volga River": "Volga River.jpg",
+        "Zeebrugge": "Zeebrugge.jpg"
+    }
