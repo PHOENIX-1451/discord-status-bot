343c48fe4ca1c113e15d28cdc3332efa4c36aa11
PHOENIX <phoenix1451.gaming@gmail.com>
2025-08-30 20:38:19 +0100

    close async session & discord connection when signal is sent by docker

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
+import signal
 import discord
 
 from src.status_bot import StatusBot
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Set intents
     intents = discord.Intents.default()
 
     # Create bot object
-    status_bot = StatusBot(intents = intents)
+    status_bot = StatusBot(intents=intents)
+
+    def shutdown_bot(signum, frame):
+        print(f"Signal {signum} received, closing bot...")
+        status_bot.loop.create_task(status_bot.close())
+
+    signal.signal(signal.SIGTERM, shutdown_bot)
+    signal.signal(signal.SIGINT, shutdown_bot)
 
     # Start bot
     status_bot.run(os.getenv("DISCORD_BOT_TOKEN"))
